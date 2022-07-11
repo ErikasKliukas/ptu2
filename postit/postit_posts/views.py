c4338e9aad1d46442becc8ff7dd50af81641a9d8
Erikas Kliukas <erikaskliukas@gmail.com>
2022-07-11 12:22:35 +0300

    detail and delete works as intended most likely

--- postit/postit_posts/views.py
+++ postit/postit_posts/views.py
@@ -1,6 +1,8 @@
 
 from django.shortcuts import render
+from django.utils.translation import gettext_lazy as _
 from rest_framework import generics, permissions
+from rest_framework.exceptions import ValidationError
 from . import models, serializers
 
 
@@ -12,6 +14,16 @@ class PostList(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
-
-
+class PostDetail(generics.RetrieveDestroyAPIView):
+    queryset = models.Post.objects.all()
+    serializer_class = serializers.PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    
+    def delete(self, request, *args, **kwargs):
+        post = post.objects.filter(pk=kwargs['pk'], user = request.user)
+        if post.exits():
+            return self.destroy(request, *args, **kwargs)
+        else:
+            return ValidationError(_("You cannot delete post which are not yours!"))
+            
 
